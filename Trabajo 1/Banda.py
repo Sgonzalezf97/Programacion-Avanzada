6195518983262c1439b145c4eb589c98220b3fdf
Juan Sebastian Gonzalez Forero <72105720+Sgonzalezf97@users.noreply.github.com>
2020-10-08 14:34:32 -0500

    Add files via upload
    Cambios agregados para el correcto funcionamiento

--- Trabajo 1/Banda.py
+++ Trabajo 1/Banda.py
@@ -7,11 +7,12 @@ from Bajo import Bajo
 
 
 class Banda():
+    def __init__(self):
+        self.musicos= []
 
     def agregarm(self, nombre):
-        lista = []
-        lista.append(nombre)
-        print(lista)
+        self.musicos.append(nombre)
+
 
     def GenerarI(self):
         numero = random.randint(1, 3)
@@ -34,23 +35,30 @@ class Banda():
             print(i.tocarEn("Do"))"""
             return i
 
+    def PresentarB(self):
+        for m in  self.musicos:
+            print (m.presentar())
+            m.tocarI(self.GenerarI())
+
 
 class Musico(Persona, Banda):
     def presentar(self):
         return "hola mi nombre es " + self.nombre + " y estoy:"
 
-    def tocarI(self):
-        b=Banda()
-        return b.GenerarI()
+    def tocarI(self,i):
+
         print(i.afinar())
         print(i.tocar())
         print(i.tocarEn("Do"))
 
 
+b=Banda()
+b.agregarm(Musico("Andres"))
+b.PresentarB()
 
-p = Musico("Andres")
+"""p = Musico("Andres")
 print(p.presentar())
 print(p.tocarI())
 j = Musico("Julio")
 print(j.presentar())
-print(j.tocarI())
+print(j.tocarI())"""
